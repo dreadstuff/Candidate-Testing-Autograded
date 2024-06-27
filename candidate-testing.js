f4902a483056beb50985e4771b2b6efaa440d5fe
dreadstuff <gavinkolasa@gmail.com>
2024-06-26 20:52:03 -0500

    task 2 update - push 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,19 +11,21 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["Who was the first American woman in space? ","True or false: 5 kilometer == 5000 meters? ","(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers = [];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-candidateName = input.prompt("Please enter your name:")
+  candidateName = input.question("Please enter your name: ")
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = prompt(question)
+  for (let i = 0; i < questions.length; i++) {
+    candidateAnswers.push(input.question(questions[i]));
+  }
 
 }
 
@@ -31,7 +33,29 @@ function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
+console.log("\nTest Results")
 
+let totalCorrect = [];
+let totalWrong = [];
+
+//looping to check answers
+
+for (let i = 0; i < correctAnswers.length; i++) {
+  let result;
+  if (candidateAnswers[i].toLowerCase() == correctAnswers[i].toLowerCase()) {
+    result = "Correct";
+    totalCorrect.push(`Question ${Number(i)} ${result}`);
+  } else {
+    result = "Incorrect";
+    totalWrong.push(`Question ${Number[i]} ${result}`);
+}
+
+console.log(`
+  Question ${i + 1}: "${questions[i].slice(0, -1)}"
+  Your answer: ${candidateAnswer[i]}
+  Correct Answer: ${correctAnswer[i]}
+  Result: ${result}`)
+}
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
